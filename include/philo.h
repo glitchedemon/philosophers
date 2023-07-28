24752e15f5635510dd76a06dff6a891b2d392533
Leonardo Fai <lfai@c2r2p7.42firenze.it>
2023-07-28 16:19:15 +0200

    norminette done v2

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: lfai <lfai@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 14:34:18 by lfai              #+#    #+#             */
-/*   Updated: 2023/07/12 17:09:01 by lfai             ###   ########.fr       */
+/*   Updated: 2023/07/28 16:19:02 by lfai             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,67 +22,86 @@
 
 struct	s_philo;
 
+/*!
+ * @brief all data needed for the routine
+ * @param n_philo number of philosophers
+ * @param full_philo philos that have already eaten
+ * @param meals number of meals
+ * @param philo pointer to s_philo
+ * @param forks mutex for the forks
+ * @param print mutex for printing
+ * @param death mutex when a philo dies
+ * @param eat mutex when eating
+ * @param the_end true when a philo dies
+ */
 typedef struct s_monitor
 {
-	struct s_philo 	*philo;
-	time_t		time_start;
-	suseconds_t		utime_start;
-	int		n_philo; //num of philos
-	int		full_philo;
-	int		meals;
-	int		time_to_die;
-	int		time_to_sleep;
-	int		time_to_eat;
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	print;
-	pthread_mutex_t	death;
-	pthread_mutex_t	eat;
-	int		the_end; //true when a philo dies, stops the routine
+	struct s_philo		*philo;
+	time_t				time_start;
+	suseconds_t			utime_start;
+	int					n_philo;
+	int					full_philo;
+	int					meals;
+	int					time_to_die;
+	int					time_to_sleep;
+	int					time_to_eat;
+	int					the_end;
+	pthread_mutex_t		*forks;
+	pthread_mutex_t		print;
+	pthread_mutex_t		death;
+	pthread_mutex_t		eat;
 }	t_monitor;
 
+/*!
+ * @brief philosophers' kit
+ * @param id philo's name
+ * @param th thread
+ * @param meal_count counter for the meals left
+ * @param last_meal the moment when last meal was eaten
+ * @param monitor pointer to s_monitor
+ * @param r_fork mutex for the right fork
+ * @param l_fork mutex for the left fork
+ */
 typedef struct s_philo
 {
-	int		id; //philo's name
-	pthread_t	th;
-	time_t		time_start;
-	suseconds_t		utime_start;
-	int		meals; //num of meals eaten
-	int		meal_count;
-	int		*full_philo;
-	int		last_meal; // the time when last meal was eaten
-	int		n_philo;
-	int		time_to_die;
-	int		time_to_sleep;
-	int		time_to_eat;
-	//int		*full_philos;
-	int		*the_end;
-	t_monitor	*monitor; //access to monitor struct
-	pthread_mutex_t	*r_fork;
-	pthread_mutex_t	*l_fork;
-	pthread_mutex_t	*print;
-	pthread_mutex_t	*death;
-	pthread_mutex_t	*eat;
-
+	int					id;
+	pthread_t			th;
+	time_t				time_start;
+	suseconds_t			utime_start;
+	int					meals;
+	int					meal_count;
+	int					*full_philo;
+	int					last_meal;
+	int					n_philo;
+	int					time_to_die;
+	int					time_to_sleep;
+	int					time_to_eat;
+	int					*the_end;
+	t_monitor			*monitor;
+	pthread_mutex_t		*r_fork;
+	pthread_mutex_t		*l_fork;
+	pthread_mutex_t		*print;
+	pthread_mutex_t		*death;
+	pthread_mutex_t		*eat;
 }	t_philo;
 
-
-
-int	main(int argc, char **argv);
-long	ft_atoi(char *str);
-void	init_thread(t_monitor *monitor);
-int	init_forks(t_monitor *monitor);
-void	init_philos(t_monitor *monitor);
-t_monitor	*init_monitor(char **argv);
-void	mutex_printer(t_philo *ph, char c);
-void	accurate_sleep(t_philo *ph, int tm);
+int				main(int argc, char **argv);
+long			ft_atoi(char *str);
+void			init_thread(t_monitor *monitor);
+int				init_forks(t_monitor *monitor);
+void			init_philos(t_monitor *monitor);
+t_monitor		*init_monitor(char **argv);
 unsigned long	get_time(t_philo *ph);
-void	*routine(void *ptr);
-void	the_death(t_monitor *monitor);
-int	check_end(t_philo *ph);
-int	check_full_philos(t_philo *ph);
-void	clear_monitor(t_monitor *monitor);
-void    clear_mutex(t_monitor *monitor);
-
-
+void			*check_monitor(char **argv, t_monitor *monitor);
+void			mutex_printer(t_philo *ph, char c);
+void			accurate_sleep(t_philo *ph, int tm);
+void			*routine(void *ptr);
+void			routine_helper(t_philo *ph);
+void			the_death(t_monitor *monitor);
+void			death_2(t_monitor *monitor, int *i);
+int				check_end(t_philo *ph);
+int				check_full_philos(t_philo *ph);
+void			clear_monitor(t_monitor *monitor);
+void			clear_all(t_monitor *monitor);
 
 #endif
